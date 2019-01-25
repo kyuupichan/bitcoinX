a62e0b6669eeb4b4acd15348aaf801d6d4f8bd3a
kyuupichan@gmail.com <kyuupichan@gmail.com>
2019-01-25 01:05:49 +0000

    Prepare 0.0.5

--- bitcoinx/__init__.py
+++ bitcoinx/__init__.py
@@ -4,7 +4,7 @@ from .hashes import *
 from .packing import *
 from .work import *
 
-_version_str = '0.0.4'
+_version_str = '0.0.5'
 _version = tuple(int(part) for part in _version_str.split('.'))
 
 __all__ = sum((
