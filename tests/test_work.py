3dbfab615a3cb242be2fc426e2c3c1be373b6662
kyuupichan@gmail.com <kyuupichan@gmail.com>
2019-01-25 01:05:16 +0000

    Add some mainnet header tests

--- tests/test_work.py
+++ tests/test_work.py
@@ -1,6 +1,16 @@
+import os
+
 import pytest
 
+from bitcoinx import (
+    HeaderStorage, CheckPoint, Bitcoin, Headers, unpack_le_uint32, pack_le_uint32,
+)
 from bitcoinx.work import *
+
+from .test_chain import create_or_open_storage
+
+
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
 
 @pytest.mark.parametrize("bits,answer", (
@@ -72,3 +82,41 @@ def test_target_to_bits_out_of_range(target):
 ))
 def test_bits_to_work(bits, answer):
     assert bits_to_work(bits) == answer
+
+
+def setup_headers(tmpdir, headers_file):
+    with open(os.path.join(data_dir, headers_file), 'rb') as f:
+        raw_headers = f.read()
+
+    last_height, = unpack_le_uint32(raw_headers[-84:-80])
+    last_raw = raw_headers[-80:]
+    checkpoint = CheckPoint(last_raw, last_height, 0)
+    storage = create_or_open_storage(tmpdir, checkpoint)
+    headers = Headers(Bitcoin, storage)
+    for offset in range(0, len(raw_headers), 84):
+        height, = unpack_le_uint32(raw_headers[offset: offset + 4])
+        raw_header = raw_headers[offset + 4: offset + 84]
+        headers.set_one(height, raw_header)
+    return headers
+
+
+def test_mainnet_2016_headers(tmpdir):
+    # Mainnet headers 0, 2015, 2016, 4031, 4032, ... 4249808
+    headers = setup_headers(tmpdir, 'mainnet-headers-2016')
+
+    chain = headers.chains()[0]
+    for height in range(0, len(headers), 2016):
+        header = headers.header_at_height(chain, height)
+        assert headers.required_bits(chain, height, None) == header.bits
+        assert headers.required_bits(chain, height + 1, None) == header.bits
+
+    assert header.difficulty() == 860_221_984_436.2223
+
+    bounded_bits = 403011440
+    # Test // 4 is lower bound for the last one
+    raw_header = bytearray(headers.storage[height - 2016])
+    timestamp = Bitcoin.header_timestamp(raw_header)
+    # Add 8 weeks and a 14 seconds; the minimum to trigger it
+    raw_header[68:72] = pack_le_uint32(timestamp + 4 * 2016 * 600 + 14)
+    headers.set_one(height - 1, raw_header)
+    assert headers.required_bits(chain, height, ) == bounded_bits
