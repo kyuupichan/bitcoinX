974cfb16d8ff6db315ebabcd72cf59e71c2c420f
kyuupichan@gmail.com <kyuupichan@gmail.com>
2019-02-28 07:06:53 +0000

    Prepare 0.0.6

--- bitcoinx/__init__.py
+++ bitcoinx/__init__.py
@@ -2,9 +2,10 @@ from .chain import *
 from .coin import *
 from .hashes import *
 from .packing import *
+from .script import *
 from .work import *
 
-_version_str = '0.0.5'
+_version_str = '0.0.6'
 _version = tuple(int(part) for part in _version_str.split('.'))
 
 __all__ = sum((
@@ -12,5 +13,6 @@ __all__ = sum((
     coin.__all__,
     hashes.__all__,
     packing.__all__,
+    script.__all__,
     work.__all__,
 ), ())
