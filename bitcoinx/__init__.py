4bbead62f75309ccc375f9559ea695ebca23632d
Neil Booth <kyuupichan@gmail.com>
2019-03-21 13:30:45 +0800

    Prepare 0.0.7
    Update setup.py

--- bitcoinx/__init__.py
+++ bitcoinx/__init__.py
@@ -9,7 +9,7 @@ from .script import *
 from .tx import *
 from .work import *
 
-_version_str = '0.0.6'
+_version_str = '0.0.7'
 _version = tuple(int(part) for part in _version_str.split('.'))
 
 __all__ = sum((
