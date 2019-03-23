3b0785365f7c68c0e03392ce6916b1badfceb25b
Neil Booth <kyuupichan@gmail.com>
2019-03-23 22:06:03 +0800

    Add compact_signature_to_der
    The complement to der_signature_to_compact

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -37,15 +37,25 @@ def test_CURVE_ORDER():
         115792089237316195423570985008687907852837564279074904382605163141518161494337
 
 
-def test_der_signature_to_compact():
-    der_sig = bytes.fromhex(
-        '30450221008dc02fa531a9a704f5c01abdeb58930514651565b42abf94f6ad1565d0ad'
-        '6785022027b1396f772c696629a4a09b01aed2416861aeaee05d0ff4a2e6fdfde73ec84d'
-    )
-    assert der_signature_to_compact(der_sig) == bytes.fromhex(
-        '8dc02fa531a9a704f5c01abdeb58930514651565b42abf94f6ad1565d0ad6785'
-        '27b1396f772c696629a4a09b01aed2416861aeaee05d0ff4a2e6fdfde73ec84d'
-    )
+# List of (der_sig, compact_sig)
+serialization_testcases = [
+    ('30450221008dc02fa531a9a704f5c01abdeb58930514651565b42abf94f6ad1565d0ad'
+     '6785022027b1396f772c696629a4a09b01aed2416861aeaee05d0ff4a2e6fdfde73ec84d',
+     '8dc02fa531a9a704f5c01abdeb58930514651565b42abf94f6ad1565d0ad6785'
+     '27b1396f772c696629a4a09b01aed2416861aeaee05d0ff4a2e6fdfde73ec84d'),
+]
+
+
+@pytest.mark.parametrize("der_sig,compact_sig", serialization_testcases)
+def test_der_signature_to_compact(der_sig, compact_sig):
+    der_sig = bytes.fromhex(der_sig)
+    assert der_signature_to_compact(der_sig) == bytes.fromhex(compact_sig)
+
+
+@pytest.mark.parametrize("der_sig,compact_sig", serialization_testcases)
+def test_compact_signature_to_der(der_sig, compact_sig):
+    compact_sig = bytes.fromhex(compact_sig)
+    assert compact_signature_to_der(compact_sig) == bytes.fromhex(der_sig)
 
 
 class TestPrivateKey:
