0ff8e6eb56fe18acfc8cd29d2043a3516c7f5cf6
Neil Booth <kyuupichan@gmail.com>
2019-04-09 16:00:52 +0100

    Prepare 0.1.3

--- bitcoinx/__init__.py
+++ bitcoinx/__init__.py
@@ -11,7 +11,7 @@ from .script import *
 from .tx import *
 from .work import *
 
-_version_str = '0.1.2'
+_version_str = '0.1.3'
 _version = tuple(int(part) for part in _version_str.split('.'))
 
 __all__ = sum((
