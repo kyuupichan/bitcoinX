c53e0b1c7e3543a1d4b329ba26c12cd6b386d494
Roger Taylor <32600614+rt121212121@users.noreply.github.com>
2019-04-17 21:19:52 +1200

    Use pycryptodomex for AES encryption/decryption if available, can red… (#4)
    * Use pycryptodomex for AES encryption/decryption if available
    
    It can reduce a 1 minute wallet load to instant.
    
    Add CI dependency of pycryptodomex

---
+++ tests/test_aes.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+
+from bitcoinx import aes
+
+class TestAES:
+    def _encrypt_decrypt(self):
+        aeskey = os.urandom(32)
+        aes_key = aeskey[:16]
+        aes_iv = aeskey[16:]
+        original_values = [
+            b"Now is the time for all good men to come to the aid of the party",
+            b"The quick brown fox jumped over the lazy dog",
+        ]
+        for original_value in original_values:
+            encrypted_value = aes.aes_encrypt_with_iv(aes_key, aes_iv, original_value)
+            value = aes.aes_decrypt_with_iv(aes_key, aes_iv, encrypted_value)
+            assert value == original_value
+
+    def test_encrypt_python(self):
+        original_AES = aes.AES
+        aes.AES = None
+        try:
+            self._encrypt_decrypt()
+        finally:
+            aes.AES = original_AES
+
+    def test_encrypt_optimized(self):
+        # This is set if pycryptodomex was found.
+        if aes.AES is None:
+            pytest.skip("pycryptodomex not installed")
+        else:
+            self._encrypt_decrypt()
