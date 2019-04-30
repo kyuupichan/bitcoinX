d5b0e461859c8046f3d67694736cd40c2547d910
kyuupichan@gmail.com <kyuupichan@gmail.com>
2019-04-30 13:26:42 +0000

    ScriptSignaure: add __eq__ and __hash__ methods

--- bitcoinx/script.py
+++ bitcoinx/script.py
@@ -319,8 +319,8 @@ class Script:
 
     def __eq__(self, other):
         '''A script equals anything buffer-like with the same bytes representation.'''
-        return (isinstance(other, (Script, bytes, bytearray, memoryview))
-                and bytes(self) == bytes(other))
+        return (isinstance(other, (bytes, bytearray, memoryview))
+                or hasattr(other, '__bytes__')) and bytes(self) == bytes(other)
 
     def _default_script(self):
         raise NotImplementedError
