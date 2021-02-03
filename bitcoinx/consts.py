7dd4f8fe69277b3b2e9f129261f590ede85f3b12
kyuupichan@gmail.com <kyuupichan@gmail.com>
2021-02-03 21:59:57 +0000

    Option to show sighash flags in to_json() output.

--- bitcoinx/consts.py
+++ bitcoinx/consts.py
@@ -34,13 +34,15 @@ from enum import IntFlag
 
 class JSONFlags(IntFlag):
     '''Flags controlling conversion of transactions and scripts to JSON.'''
-    # Include the transaction size in bytes
-    SIZE = 1 << 1
-    # Include a human-readable description of the locktime constraint is output
-    LOCKTIME_MEANING = 1 << 2
     # Include the index of each input
-    ENUMERATE_INPUTS = 1 << 3
+    ENUMERATE_INPUTS = 1 << 0
     # Include the index of each output
-    ENUMERATE_OUTPUTS = 1 << 4
+    ENUMERATE_OUTPUTS = 1 << 1
+    # Include the transaction size in bytes
+    SIZE = 1 << 2
+    # Include a human-readable description of the locktime constraint is output
+    LOCKTIME_MEANING = 1 << 3
     # Include classification of output scripts
-    CLASSIFY_OUTPUT_SCRIPT = 1 << 5
+    CLASSIFY_OUTPUT_SCRIPT = 1 << 4
+    # Display signature sighashes as text
+    SIGHASH_MEANING = 1 << 5
