65b3ea8d0c1e9fdc0767d50446a7fb3ee532e6b5
kyuupichan@gmail.com <kyuupichan@gmail.com>
2021-02-17 14:01:19 +0000

    Implement some easy opcodes.

--- bitcoinx/script.py
+++ bitcoinx/script.py
@@ -803,11 +803,8 @@ def evaluate_script(state, script):
             state.validate_minimal_push_opcode(op, item)
             state.stack.append(op)
         elif execute or Ops.OP_IF <= op <= Ops.OP_ENDIF:
-            #
-            # Stuff
-            #
-            if op == OP_NOP:
-                pass
+            op_handlers[op](state)
+
             # Post-genesis UTXOs permit OP_VER in unexecuted branches
             elif op in {OP_VERNOTIF, OP_VERIF} and state.is_utxo_after_genesis and not execute:
                 pass
@@ -818,3 +815,123 @@ def evaluate_script(state, script):
 
     if state.execute_stack:
         raise UnclosedConditionals('unterminated if/else/endif condition evaluating script')
+
+
+def invalid_opcode(_state, op):
+    raise InvalidOpcode(f'invalid opcode: {op}')
+
+
+#
+# Control ops
+#
+
+def handle_NOP(_state):
+    pass
+
+
+def handle_DUP(state):
+    state.require_depth(1)
+    state.stack.append(state.stack[-1])
+
+
+
+
+op_handlers = [partial(invalid_opcode, op=op) for op in range(256)]
+op_handlers[OP_DUP] = handle_DUP
+op_handlers[OP_NOP] = handle_NOP
+
+    # OP_TOALTSTACK = 0x6b
+    # OP_FROMALTSTACK = 0x6c
+    # OP_2DROP = 0x6d
+    # OP_2DUP = 0x6e
+    # OP_3DUP = 0x6f
+    # OP_2OVER = 0x70
+    # OP_2ROT = 0x71
+    # OP_2SWAP = 0x72
+    # OP_IFDUP = 0x73
+    # OP_DEPTH = 0x74
+    # OP_DROP = 0x75
+    # OP_DUP = 0x76
+    # OP_NIP = 0x77
+    # OP_OVER = 0x78
+    # OP_PICK = 0x79
+    # OP_ROLL = 0x7a
+    # OP_ROT = 0x7b
+    # OP_SWAP = 0x7c
+    # OP_TUCK = 0x7d
+
+    # # splice ops
+    # OP_CAT = 0x7e
+    # OP_SPLIT = 0x7f    # after monolith upgrade (May 2018)
+    # OP_NUM2BIN = 0x80  # after monolith upgrade (May 2018)
+    # OP_BIN2NUM = 0x81  # after monolith upgrade (May 2018)
+    # OP_SIZE = 0x82
+
+    # # bit logic
+    # OP_INVERT = 0x83
+    # OP_AND = 0x84
+    # OP_OR = 0x85
+    # OP_XOR = 0x86
+    # OP_EQUAL = 0x87
+    # OP_EQUALVERIFY = 0x88
+    # OP_RESERVED1 = 0x89
+    # OP_RESERVED2 = 0x8a
+
+    # # numeric
+    # OP_1ADD = 0x8b
+    # OP_1SUB = 0x8c
+    # OP_2MUL = 0x8d
+    # OP_2DIV = 0x8e
+    # OP_NEGATE = 0x8f
+    # OP_ABS = 0x90
+    # OP_NOT = 0x91
+    # OP_0NOTEQUAL = 0x92
+
+    # OP_ADD = 0x93
+    # OP_SUB = 0x94
+    # OP_MUL = 0x95
+    # OP_DIV = 0x96
+    # OP_MOD = 0x97
+    # OP_LSHIFT = 0x98
+    # OP_RSHIFT = 0x99
+
+    # OP_BOOLAND = 0x9a
+    # OP_BOOLOR = 0x9b
+    # OP_NUMEQUAL = 0x9c
+    # OP_NUMEQUALVERIFY = 0x9d
+    # OP_NUMNOTEQUAL = 0x9e
+    # OP_LESSTHAN = 0x9f
+    # OP_GREATERTHAN = 0xa0
+    # OP_LESSTHANOREQUAL = 0xa1
+    # OP_GREATERTHANOREQUAL = 0xa2
+    # OP_MIN = 0xa3
+    # OP_MAX = 0xa4
+
+    # OP_WITHIN = 0xa5
+
+    # # crypto
+    # OP_RIPEMD160 = 0xa6
+    # OP_SHA1 = 0xa7
+    # OP_SHA256 = 0xa8
+    # OP_HASH160 = 0xa9
+    # OP_HASH256 = 0xaa
+    # OP_CODESEPARATOR = 0xab
+    # OP_CHECKSIG = 0xac
+    # OP_CHECKSIGVERIFY = 0xad
+    # OP_CHECKMULTISIG = 0xae
+    # OP_CHECKMULTISIGVERIFY = 0xaf
+
+    # # expansion
+    # OP_NOP1 = 0xb0
+    # OP_CHECKLOCKTIMEVERIFY = 0xb1
+    # OP_NOP2 = OP_CHECKLOCKTIMEVERIFY
+    # OP_CHECKSEQUENCEVERIFY = 0xb2
+    # OP_NOP3 = OP_CHECKSEQUENCEVERIFY
+    # OP_NOP4 = 0xb3
+    # OP_NOP5 = 0xb4
+    # OP_NOP6 = 0xb5
+    # OP_NOP7 = 0xb6
+    # OP_NOP8 = 0xb7
+    # OP_NOP9 = 0xb8
+    # OP_NOP10 = 0xb9
+]
