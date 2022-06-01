76f165a4ba8005ccc53cf2ee103e2b198040682a
kyuupichan@gmail.com <kyuupichan@gmail.com>
2022-06-01 11:14:20 +0000

    Prepare 0.7.0

--- bitcoinx/__init__.py
+++ bitcoinx/__init__.py
@@ -17,7 +17,7 @@ from .signature import *
 from .tx import *
 from .work import *
 
-_version_str = '0.6.0'
+_version_str = '0.7.0'
 _version = tuple(int(part) for part in _version_str.split('.'))
 
 __all__ = sum((
